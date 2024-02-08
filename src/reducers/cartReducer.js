215642ef5c56c80c59f52b4265473fddeace6a4f
Nelson Toro <nelson.toro.flores@gmail.com>
2024-02-08 19:17:02 -0400

    Use localStorage to save the cart context

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,9 +1,15 @@
-export const cartInitialState = []
+export const cartInitialState =
+  JSON.parse(window.localStorage.getItem('cart')) || []
 
 export const CART_ACTION_TYPES = {
   ADD_TO_CART: 'ADD_TO_CART',
   REMOVE_FROM_CART: 'REMOVE_FROM_CART',
   CLEAR_CART: 'CLEAR_CART'
+}
+
+// update localStorage with state for cart
+export const updateLocalStorage = (state) => {
+  window.localStorage.setItem('cart', JSON.stringify(state))
 }
 
 export const cartReducer = (state, action) => {
@@ -18,24 +24,30 @@ export const cartReducer = (state, action) => {
         // create a new cart in order to modify it for later set that modification to the state
         const newState = structuredClone(state)
         newState[productInTheCartIndex].quantity += 1
+        updateLocalStorage(newState)
         return newState
       }
 
-      return [
+      const newState = [
         ...state,
         {
           ...actionPayload, // product
           quantity: 1
         }
       ]
+      updateLocalStorage(newState)
+      return newState
     }
 
     case CART_ACTION_TYPES.REMOVE_FROM_CART: {
       const { id } = actionPayload
-      return state.filter((item) => item !== id)
+      const newState = state.filter((item) => item !== id)
+      updateLocalStorage(newState)
+      return newState
     }
 
     case CART_ACTION_TYPES.CLEAR_CART: {
+      updateLocalStorage(cartInitialState)
       return cartInitialState
     }
   }
